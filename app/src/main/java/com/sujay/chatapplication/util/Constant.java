337d1f23e6718913e7e553c5f36d05442889fcfd
Sujay Naik <sujaynaik@users.noreply.github.com>
2017-08-23 11:32:55 +0530

    Added utils

---
+++ app/src/main/java/com/sujay/chatapplication/util/Constant.java
@@ -0,0 +1,37 @@
+package com.example.sujaynaik.myapplication.util;
+
+/**
+ * Created by sujaynaik on 5/23/17.
+ */
+
+public class Constant {
+    /***
+     * COMMON
+     */
+    public static final String APP_NAME = "sujayapp";
+    public static final String DB_NAME = "db_my_application";
+    public static final int DB_VERSION = 1;
+    public static final boolean DEBUG_ANIM = true;
+    static boolean DEBUG = true;
+
+    /**
+     * SERVER
+     */
+    static final String BASE_URL = "http://test.com/";
+    static final String API_REGISTER = "register";
+    static final String API_LOGIN = "login";
+
+    /**
+     * INTENT
+     */
+    public static String INTENT_ID = "id";
+    public static String INTENT_FNAME = "fname";
+    public static String INTENT_LNAME = "lname";
+
+    /**
+     * THEMES
+     */
+    public static int CURRENT_THEME = 1;
+    public static final int THEME_ORIGINAL = 1;
+    public static final int THEME_BLUE = 2;
+}
---
+++ app/src/main/java/com/sujay/chatapplication/util/MarshmallowPermissions.java
@@ -0,0 +1,110 @@
+package com.example.sujaynaik.myapplication.util;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
+/**
+ * Created by sujaynaik on 7/4/17.
+ */
+
+public class MarshmallowPermissions {
+
+    public static final int EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE = 1;
+    public static final int CAMERA_PERMISSION_REQUEST_CODE = 2;
+    public static final int CONTACTS_PERMISSION_REQUEST_CODE = 3;
+    public static final int FINE_LOCATION_PERMISSION_REQUEST_CODE = 4;
+    public static final int COARSE_LOCATION_PERMISSION_REQUEST_CODE = 5;
+    private Activity activity;
+
+    public MarshmallowPermissions(Activity activity) {
+        this.activity = activity;
+    }
+
+    public static boolean checkPermissionForExternalStorage(Activity activity) {
+        int result = ContextCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (result == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void requestPermissionForExternalStorage(Activity activity) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE);
+//            Toast.makeText(activity, "External Storage permission needed. Please allow in App Settings for additional functionality.", Toast.LENGTH_LONG).down_from_top();
+        } else {
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    public static boolean checkPermissionForCamera(Activity activity) {
+        int result = ContextCompat.checkSelfPermission(activity, Manifest.permission.CAMERA);
+        if (result == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void requestPermissionForCamera(Activity activity) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.CAMERA)) {
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
+//            Toast.makeText(activity, "Camera permission needed. Please allow in App Settings for additional functionality.", Toast.LENGTH_LONG).down_from_top();
+        } else {
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    public static boolean checkPermissionForContacts(Activity activity) {
+        int result = ContextCompat.checkSelfPermission(activity, Manifest.permission.READ_CONTACTS);
+        if (result == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void requestPermissionForContacts(Activity activity) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.READ_CONTACTS)) {
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.READ_CONTACTS}, CONTACTS_PERMISSION_REQUEST_CODE);
+//            Toast.makeText(activity, "Camera permission needed. Please allow in App Settings for additional functionality.", Toast.LENGTH_LONG).down_from_top();
+        } else {
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.READ_CONTACTS}, CONTACTS_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    public static boolean checkPermissionForLocation(Activity activity) {
+        int result = ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION);
+        int result2 = ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_COARSE_LOCATION);
+        if (result == PackageManager.PERMISSION_GRANTED && result2 == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void requestPermissionForLocation(Activity activity) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    FINE_LOCATION_PERMISSION_REQUEST_CODE);
+//            Toast.makeText(activity, "Camera permission needed. Please allow in App Settings for additional functionality.", Toast.LENGTH_LONG).down_from_top();
+        } else {
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    FINE_LOCATION_PERMISSION_REQUEST_CODE);
+        }
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    COARSE_LOCATION_PERMISSION_REQUEST_CODE);
+//            Toast.makeText(activity, "Camera permission needed. Please allow in App Settings for additional functionality.", Toast.LENGTH_LONG).down_from_top();
+        } else {
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    COARSE_LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+}
